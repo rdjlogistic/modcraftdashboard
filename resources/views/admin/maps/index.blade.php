24bbefea799e6d34ad40289ca75da5ca70679c70
Divyesh Paun <divyesh@logisticinfotech.co.in>
2021-09-02 19:22:26 +0530

    Solve error in mods and map list

--- resources/views/admin/maps/index.blade.php
+++ resources/views/admin/maps/index.blade.php
@@ -2,29 +2,29 @@
 @section('content')
 
 <?php if(isset($_GET['app_id'])) { $appid = $_GET['app_id']; } else { $appid = ''; }?>
-    <form action="{{ route('admin.maps.index') }}" method="GET" style="margin-top: 20px;" enctype="multipart/form-data">
-        <div class="row">
-            <div class="form-group col-md-9">
-                <select name="app_id" id="input" class="form-control select2">
-                    <option value=""  selected>Select App</option>
-                    @foreach($apps as $id => $app)
-                        <option value="{{ $id }}" {{ $id == $appid ? 'selected' : '' }}>{{ $app }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="col-md-3">
-            <input type="submit" class="btn btn-danger btn-sm btn-select" value="Filter" style="padding:5px 30px;">
-            </div>
+<form action="{{ route('admin.maps.index') }}" method="GET" style="margin-top: 20px;" enctype="multipart/form-data">
+    <div class="row">
+        <div class="form-group col-md-9">
+            <select name="app_id" id="input" class="form-control select2">
+                <option value="" selected>Select App</option>
+                @foreach($apps as $id => $app)
+                <option value="{{ $id }}" {{ $id == $appid ? 'selected' : '' }}>{{ $app }}</option>
+                @endforeach
+            </select>
         </div>
-    </form>
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route("admin.maps.create") }}">
-                {{ trans('global.add') }} {{ trans('cruds.map.title_singular') }}
-            </a>
+
+        <div class="col-md-3">
+            <input type="submit" class="btn btn-danger btn-sm btn-select" value="Filter" style="padding:5px 30px;">
         </div>
     </div>
+</form>
+<div style="margin-bottom: 10px;" class="row">
+    <div class="col-lg-12">
+        <a class="btn btn-success" href="{{ route("admin.maps.create") }}">
+            {{ trans('global.add') }} {{ trans('cruds.map.title_singular') }}
+        </a>
+    </div>
+</div>
 
 <div class="card">
     <div class="card-header ">
@@ -32,7 +32,7 @@
     </div>
     <div class="table-custom-main">
         <div class="card-body">
-        
+
             <div class="table-responsive">
                 <table class=" table table-bordered table-striped table-hover datatable datatable-Map">
                     <thead>
@@ -46,7 +46,7 @@
                             <th>
                                 {{ trans('cruds.map.fields.name') }}
                             </th>
-                            <th  width="30%">
+                            <th width="30%">
                                 {{ trans('cruds.map.fields.description') }}
                             </th>
                             <th>
@@ -67,94 +67,95 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @if($maps)
                         @foreach($maps as $key => $map)
-                            <?php if($map->app->id == $appid){?>
-                            <tr data-entry-id="{{ $map->id }}">
-                                <td>
+                        <?php if($map->app->id == $appid){?>
+                        <tr data-entry-id="{{ $map->id }}">
+                            <td>
 
-                                </td>
-                                <td>
-                                    {{ $map->id ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $map->name ?? '' }}
-                                </td>
-                                <td>
-                                    {{ Str::limit($map->description, 150) }}
-                                </td>
-                                <td>
-                                    {{ $map->platform ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $map->app->name ?? '' }}
-                                </td>
-                                <!-- <td>{{ $map->filename}}</td> -->
-                                <td><img src="{{ $map->image}}" style="height:50px; width:70px;"></td>
-                                <!-- <td>
+                            </td>
+                            <td>
+                                {{ $map->id ?? '' }}
+                            </td>
+                            <td>
+                                {{ $map->name ?? '' }}
+                            </td>
+                            <td>
+                                {{ Str::limit($map->description, 150) }}
+                            </td>
+                            <td>
+                                {{ $map->platform ?? '' }}
+                            </td>
+                            <td>
+                                {{ $map->app->name ?? '' }}
+                            </td>
+                            <!-- <td>{{ $map->filename}}</td> -->
+                            <td><img src="{{ $map->image}}" style="height:50px; width:70px;"></td>
+                            <!-- <td>
                                     @if($map->logo)
                                         <a href="{{ $map->logo->getUrl() }}" target="_blank">
                                             <img src="{{ $map->logo->getUrl('thumb') }}" width="50px" height="50px">
                                         </a>
                                     @endif
                                 </td> -->
-                                <td>
-                                        <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.maps.edit', $map->id) }}">
-                                            {{ trans('global.edit') }}
-                                        </a>
-                                    
-                                        <form class="btn-delete-form" action="{{ route('admin.maps.destroy', $map->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                            <input type="hidden" name="_method" value="DELETE">
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                        </form>
-                                </td>
-                                
-                            </tr>
+                            <td>
+                                <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.maps.edit', $map->id) }}">
+                                    {{ trans('global.edit') }}
+                                </a>
+
+                                <form class="btn-delete-form" action="{{ route('admin.maps.destroy', $map->id) }}"
+                                    method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                    style="display: inline-block;">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <input type="submit" class="btn btn-xs btn-danger"
+                                        value="{{ trans('global.delete') }}">
+                                </form>
+                            </td>
+
+                        </tr>
                         <?php } else{
                             if($appid == ''){  ?>
-                            <tr data-entry-id="{{ $map->id }}">
-                                <td>
+                        <tr data-entry-id="{{ $map->id }}">
+                            <td>
 
-                                </td>
-                                <td>
-                                    {{ $map->id ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $map->name ?? '' }}
-                                </td>
-                                <td>
-                                    {{ Str::limit($map->description, 150) }}
-                                </td>
-                                <td>
-                                    {{ $map->platform ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $map->app->name ?? '' }}
-                                </td>
-                                <!-- <td>{{ $map->filename}}</td> -->
-                                <td><img src="{{ $map->image}}" style="height:50px; width:70px;"></td>
-                                <!-- <td>
-                                    @if($map->logo)
-                                        <a href="{{ $map->logo->getUrl() }}" target="_blank">
-                                            <img src="{{ $map->logo->getUrl('thumb') }}" width="50px" height="50px">
-                                        </a>
-                                    @endif
-                                </td> -->
-                                <td>
-                                        <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.maps.edit', $map->id) }}">
-                                            {{ trans('global.edit') }}
-                                        </a>
-                                    
-                                        <form class="btn-delete-form" action="{{ route('admin.maps.destroy', $map->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                            <input type="hidden" name="_method" value="DELETE">
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                        </form>
-                                </td>
-                                
-                            </tr>
+                            </td>
+                            <td>
+                                {{ $map->id ?? '' }}
+                            </td>
+                            <td>
+                                {{ $map->name ?? '' }}
+                            </td>
+                            <td>
+                                {{ Str::limit($map->description, 150) }}
+                            </td>
+                            <td>
+                                {{ $map->platform ?? '' }}
+                            </td>
+                            <td>
+                                {{ $map->app->name ?? '' }}
+                            </td>
+                            <td><img src="{{ $map->image}}" style="height:50px; width:70px;"></td>
+
+                            <td>
+                                <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.maps.edit', $map->id) }}">
+                                    {{ trans('global.edit') }}
+                                </a>
+
+                                <form class="btn-delete-form" action="{{ route('admin.maps.destroy', $map->id) }}"
+                                    method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                    style="display: inline-block;">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <input type="submit" class="btn btn-xs btn-danger"
+                                        value="{{ trans('global.delete') }}">
+                                </form>
+                            </td>
+
+                        </tr>
                         <?php } }?>
                         @endforeach
+                        @endif
                     </tbody>
                 </table>
             </div>
--- resources/views/admin/mods/index.blade.php
+++ resources/views/admin/mods/index.blade.php
@@ -2,37 +2,37 @@
 @section('content')
 
 <?php if(isset($_GET['app_id'])) { $appid = $_GET['app_id']; } else { $appid = ''; }?>
-    <form action="{{ route('admin.mods.index') }}" method="GET" style="margin-top: 20px;" enctype="multipart/form-data">
-        <div class="row">
-            <div class="form-group col-md-9">
-                <select name="app_id" id="input" class="form-control select2">
-                    <option value=""  selected>Select App</option>
-                    @foreach($apps as $id => $app)
-                        <option value="{{ $id }}" {{ $id == $appid ? 'selected' : '' }}>{{ $app }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="col-md-3">
-            <input type="submit" class="btn btn-danger btn-sm btn-select" value="Filter" style="padding:5px 30px;">
-            </div>
+<form action="{{ route('admin.mods.index') }}" method="GET" style="margin-top: 20px;" enctype="multipart/form-data">
+    <div class="row">
+        <div class="form-group col-md-9">
+            <select name="app_id" id="input" class="form-control select2">
+                <option value="" selected>Select App</option>
+                @foreach($apps as $id => $app)
+                <option value="{{ $id }}" {{ $id == $appid ? 'selected' : '' }}>{{ $app }}</option>
+                @endforeach
+            </select>
         </div>
-    </form>
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route("admin.mods.create") }}">
-                {{ trans('global.add') }} {{ trans('cruds.mod.title_singular') }}
-            </a>
+
+        <div class="col-md-3">
+            <input type="submit" class="btn btn-danger btn-sm btn-select" value="Filter" style="padding:5px 30px;">
         </div>
     </div>
+</form>
+<div style="margin-bottom: 10px;" class="row">
+    <div class="col-lg-12">
+        <a class="btn btn-success" href="{{ route("admin.mods.create") }}">
+            {{ trans('global.add') }} {{ trans('cruds.mod.title_singular') }}
+        </a>
+    </div>
+</div>
 
 <div class="card">
     <div class="card-header">
-    <h2 class="title-main">{{ trans('cruds.mod.title_singular') }} {{ trans('global.list') }}</h2>
+        <h2 class="title-main">{{ trans('cruds.mod.title_singular') }} {{ trans('global.list') }}</h2>
     </div>
     <div class="table-custom-main">
         <div class="card-body">
-        
+
             <div class="table-responsive">
                 <table class=" table table-bordered table-striped table-hover datatable datatable-Mod">
                     <thead>
@@ -67,97 +67,88 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @if($mods)
                         @foreach($mods as $key => $mod)
-                            <?php if($mod->app->id == $appid){?>
-                            <tr data-entry-id="{{ $mod->id }}">
-                                <td>
+                        <?php if($mod->app->id == $appid){?>
+                        <tr data-entry-id="{{ $mod->id }}">
+                            <td>
 
-                                </td>
-                                <td>
-                                    {{ $mod->id ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $mod->name ?? '' }}
-                                </td>
-                                <td>
-                                    <!-- {{ $mod->description ?? '' }} -->
-                                    {{ Str::limit($mod->description, 150) }}
-                                </td>
-                                <td>
-                                    {{ $mod->platform ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $mod->app->name ?? '' }}
-                                </td>
-                                <!-- <td>{{ $mod->filename}}</td> -->
-                                <td><img src="{{ $mod->image}}" style="height:50px; width:70px;"></td>
-                                <!-- <td>
-                                    @if($mod->logo)
-                                        <a href="{{ $mod->logo->getUrl() }}" target="_blank">
-                                            <img src="{{ $mod->logo->getUrl('thumb') }}" width="50px" height="50px">
-                                        </a>
-                                    @endif
-                                </td> -->
-                                <td>
-                                        <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.mods.edit', $mod->id) }}">
-                                            {{ trans('global.edit') }}
-                                        </a>
-                                    
-                                        <form class="btn-delete-form" action="{{ route('admin.mods.destroy', $mod->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                            <input type="hidden" name="_method" value="DELETE">
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                        </form>
-                                </td>
-                                
-                            </tr>
+                            </td>
+                            <td>
+                                {{ $mod->id ?? '' }}
+                            </td>
+                            <td>
+                                {{ $mod->name ?? '' }}
+                            </td>
+                            <td>
+                                {{ Str::limit($mod->description, 150) }}
+                            </td>
+                            <td>
+                                {{ $mod->platform ?? '' }}
+                            </td>
+                            <td>
+                                {{ $mod->app->name ?? '' }}
+                            </td>
+                            <td><img src="{{ $mod->image}}" style="height:50px; width:70px;"></td>
+                            <td>
+                                <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.mods.edit', $mod->id) }}">
+                                    {{ trans('global.edit') }}
+                                </a>
+
+                                <form class="btn-delete-form" action="{{ route('admin.mods.destroy', $mod->id) }}"
+                                    method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                    style="display: inline-block;">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <input type="submit" class="btn btn-xs btn-danger"
+                                        value="{{ trans('global.delete') }}">
+                                </form>
+                            </td>
+
+                        </tr>
                         <?php } else {
                             if($appid == ''){ ?>
-                            
-                            <tr data-entry-id="{{ $mod->id }}">
-                                <td>
 
-                                </td>
-                                <td>
-                                    {{ $mod->id ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $mod->name ?? '' }}
-                                </td>
-                                <td>
-                                    <!-- {{ $mod->description ?? '' }} -->
-                                    {{ Str::limit($mod->description, 150) }}
-                                </td>
-                                <td>
-                                    {{ $mod->platform ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $mod->app->name ?? '' }}
-                                </td>
-                                <!-- <td>{{ $mod->filename}}</td> -->
-                                <td><img src="{{ $mod->image}}" style="height:50px; width:70px;"></td>
-                                <!-- <td>
-                                    @if($mod->logo)
-                                        <a href="{{ $mod->logo->getUrl() }}" target="_blank">
-                                            <img src="{{ $mod->logo->getUrl('thumb') }}" width="50px" height="50px">
-                                        </a>
-                                    @endif
-                                </td> -->
-                                <td>
-                                        <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.mods.edit', $mod->id) }}">
-                                            {{ trans('global.edit') }}
-                                        </a>
-                                    
-                                        <form class="btn-delete-form" action="{{ route('admin.mods.destroy', $mod->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                            <input type="hidden" name="_method" value="DELETE">
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                        </form>
-                                </td>
-                                
-                            </tr>
+                        <tr data-entry-id="{{ $mod->id }}">
+                            <td>
+
+                            </td>
+                            <td>
+                                {{ $mod->id ?? '' }}
+                            </td>
+                            <td>
+                                {{ $mod->name ?? '' }}
+                            </td>
+                            <td>
+                                {{ Str::limit($mod->description, 150) }}
+                            </td>
+                            <td>
+                                {{ $mod->platform ?? '' }}
+                            </td>
+                            <td>
+                                {{ $mod->app->name ?? '' }}
+                            </td>
+                            <td><img src="{{ $mod->image}}" style="height:50px; width:70px;"></td>
+
+                            <td>
+                                <a class="btn btn-xs btn-info btn-edit" href="{{ route('admin.mods.edit', $mod->id) }}">
+                                    {{ trans('global.edit') }}
+                                </a>
+
+                                <form class="btn-delete-form" action="{{ route('admin.mods.destroy', $mod->id) }}"
+                                    method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                    style="display: inline-block;">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <input type="submit" class="btn btn-xs btn-danger"
+                                        value="{{ trans('global.delete') }}">
+                                </form>
+                            </td>
+
+                        </tr>
                         <?php } }?>
                         @endforeach
+                        @endif
                     </tbody>
                 </table>
             </div>
