69b94636ad8f594415ff0371881424c165a61a5d
Divyesh Paun <divyesh@logisticinfotech.co.in>
2021-09-06 14:51:00 +0530

    Changes in skin listing api

--- app/Http/Controllers/Api/V1/Admin/SkinsApiController.php
+++ app/Http/Controllers/Api/V1/Admin/SkinsApiController.php
@@ -22,49 +22,34 @@ class SkinsApiController extends Controller
     public function index()
     {
         $skins = Skin::all();
-    
+
         return $this->sendResponse(SkinResource::collection($skins), 'Skins retrieved successfully.');
     }
 
     public function getSkinsByApp(Request $request)
     {
-       
         $platforms = [$request->platform,'both'];
-        if($request->page && $request->search == ''){
-            if($request->platform == 'both'){
-                $skins = Skin::where('app_id', $request->app_id )->paginate(10);
+        if ($request->page && $request->search == '') {
+            if ($request->platform == 'both') {
+                $skins = Skin::where('app_id', $request->app_id)->paginate(10);
+            } else {
+                $skins = Skin::where('app_id', $request->app_id)->whereIn('platform', $platforms)->paginate(10);
             }
-            else{
-                $skins = Skin::where('app_id', $request->app_id )->whereIn('platform' , $platforms)->paginate(10);
-            }
-            
-        }
-        else if($request->platform == 'both'){
-            $skins = Skin::where('app_id', $request->app_id )->paginate(10);
-        
-        }
-        else if($request->search){
-            if($request->platform){
-                if($request->platform == 'both'){
-                    
-                    $skins = Skin::where('name','LIKE','%'.$request->search.'%')->paginate(10); 
-                }else{
-
-                    $skins = Skin::where('name','LIKE','%'.$request->search.'%')->whereIn('platform' , $platforms)->paginate(10);
-
+        } elseif ($request->platform == 'both') {
+            $skins = Skin::where('app_id', $request->app_id)->paginate(10);
+        } elseif ($request->search) {
+            if ($request->platform) {
+                if ($request->platform == 'both') {
+                    $skins = Skin::where('app_id', $request->app_id)->where('name', 'LIKE', '%'.$request->search.'%')->paginate(10);
+                } else {
+                    $skins = Skin::where('app_id', $request->app_id)->where('name', 'LIKE', '%'.$request->search.'%')->whereIn('platform', $platforms)->paginate(10);
                 }
+            } else {
+                $skins = Skin::where('name', 'LIKE', '%'.$request->search.'%')->paginate(10);
             }
-            else{
-            $skins = Skin::where('name','LIKE','%'.$request->search.'%')->paginate(10); 
-            }
-        }
-        else{
-            
-            $skins = Skin::where('app_id', $request->app_id )->whereIn('platform' , $platforms)->paginate(10);
+        } else {
+            $skins = Skin::where('app_id', $request->app_id)->whereIn('platform', $platforms)->paginate(10);
         }
         return Response::json(array('data' => $skins));
-
     }
-
-    
 }
